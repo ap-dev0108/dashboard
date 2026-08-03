db48f3cd160e524828079d3e7bea941a9ffec566
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-03 16:26:35 +0545

    refactor(interface): Refactoring return values

--- server/Application/Interface/IMediaRepo.cs
+++ server/Application/Interface/IMediaRepo.cs
@@ -5,7 +5,7 @@ namespace server.Application;
 public interface IMediaRepo
 {
     Task<List<Media>> GetAllMediasAsync();
-    Task<Media> AddMedia(Media media);
+    void AddMedia(Media media);
     Task<Media> GetMediaById(Guid id);
-    Task<Media> RemoveMedia(Media media);
+    void RemoveMedia(Media media);
 }
