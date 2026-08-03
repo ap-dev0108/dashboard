109681db7b6bab4df5d056611e80cc78d04dacd5
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-03 16:31:43 +0545

    config(injection): Injecting the global DI class into the main file

--- server/Program.cs
+++ server/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
+using server.Infra;
 using server.Infra.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.RepoDI();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
