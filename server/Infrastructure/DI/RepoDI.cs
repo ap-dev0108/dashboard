a97080b06ae498e7853aaa1c2c01cf14962ec603
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-03 16:31:03 +0545

    config(injection): Creating a global file for repo injection

---
+++ server/Infrastructure/DI/RepoDI.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
+using server.Application;
+
+namespace server.Infra;
+
+public static class RepoInjection
+{
+    public static IServiceCollection RepoDI(this IServiceCollection services)
+    {
+        services.AddScoped<IMediaRepo, MediaRepo>();
+        services.AddScoped<MediaRepo>();
+
+        return services;
+    }
+}
