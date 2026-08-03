7f964354888db30df81ab781844ca7692da45a9e
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-03 14:24:48 +0545

    chore(enum): Creating enum for statuses

---
+++ server/Domain/Enum/Enum.cs
@@ -0,0 +1,17 @@
+namespace server.Domain;
+
+public enum MediaType
+{
+    Movies = 0,
+    TVShows = 1,
+    Books = 2,
+    Games = 3,
+    Music = 4
+}
+
+public enum MediaStatus
+{
+    Finished = 0,
+    Planning = 1,
+    InProgess = 2
+}
