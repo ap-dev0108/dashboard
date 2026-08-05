26db5e803bc4f8dbfd3af04835df71f4ed897cc0
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-05 10:47:16 +0545

    refactor(entity): Updating datatype for column Ratings from integer to double

--- server/Domain/Entities/Media.cs
+++ server/Domain/Entities/Media.cs
@@ -14,7 +14,7 @@ public class Media
 
     public string ImageURL { get; set; } = string.Empty;
 
-    public int Ratings { get; set; }
+    public double Ratings { get; set; }
 
     public MediaType Type { get; set; }
     public MediaStatus Status { get; set; }
