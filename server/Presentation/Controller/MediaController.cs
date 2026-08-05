eb89334a6b1f30d40ee8d764f647ea387c37537b
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-05 10:34:30 +0545

    chore(api): Creating apis for the crud operation

--- server/Presentation/Controller/MediaController.cs
+++ server/Presentation/Controller/MediaController.cs
@@ -4,7 +4,8 @@ using server.Application;
 namespace server.Presentation;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
+[ApiExplorerSettings(IgnoreApi = false)]
 public class MediaController : ControllerBase
 {
     private readonly MediaService _mediaServices;
@@ -14,11 +15,31 @@ public class MediaController : ControllerBase
         _mediaServices = mediaService;
     }
 
-    [HttpGet("allMedias")]
+    [HttpGet("/allMedias")]
     public async Task<IActionResult> GetAllMedias()
     {
         var media = await _mediaServices.GetMediaDTOsAsync();
-
         return Ok(media);
+    }
+
+    [HttpGet("/id")]
+    public async Task<IActionResult> GetMediaById(Guid id)
+    {
+        var media = await _mediaServices.GetMediaById(id);
+        return Ok(media);
+    }
+
+    [HttpPost("/addMedia")]
+    public async Task<IActionResult> AddMedias(AddMediaDTO mediaDTO)
+    {
+        await _mediaServices.AddMedia(mediaDTO);
+        return Ok(mediaDTO);
+    }
+
+    [HttpDelete("/removeMedia")]
+    public async Task<IActionResult> RemoveMedias(Guid id)
+    {
+        await _mediaServices.RemoveMedia(id);
+        return Ok(id);
     }
 }
