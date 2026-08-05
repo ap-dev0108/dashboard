6427ad41355ce96c1b4cefa836896091dd990b37
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-05 10:48:25 +0545

    refactor(response): Refactoring responses by wrapping them into Response dto

--- server/Presentation/Controller/MediaController.cs
+++ server/Presentation/Controller/MediaController.cs
@@ -19,27 +19,51 @@ public class MediaController : ControllerBase
     public async Task<IActionResult> GetAllMedias()
     {
         var media = await _mediaServices.GetMediaDTOsAsync();
-        return Ok(media);
+
+        return Ok(new Response<List<MediaDTO>>
+        {
+            Success = true,
+            Message = "Media List fetched Successfully",
+            Data = media
+        });
     }
 
     [HttpGet("/id")]
     public async Task<IActionResult> GetMediaById(Guid id)
     {
         var media = await _mediaServices.GetMediaById(id);
-        return Ok(media);
+
+        return Ok(new Response<MediaDTO>
+        {
+            Success = true,
+            Message = "Media data fetched",
+            Data = media
+        });
     }
 
     [HttpPost("/addMedia")]
     public async Task<IActionResult> AddMedias(AddMediaDTO mediaDTO)
     {
         await _mediaServices.AddMedia(mediaDTO);
-        return Ok(mediaDTO);
+
+        return Ok(new Response<AddMediaDTO>
+        {
+            Success = true,
+            Message = "Media added",
+            Data = mediaDTO
+        });
     }
 
     [HttpDelete("/removeMedia")]
     public async Task<IActionResult> RemoveMedias(Guid id)
     {
         await _mediaServices.RemoveMedia(id);
-        return Ok(id);
+
+        return Ok(new Response<Guid>
+        {
+            Success = true,
+            Message = "Media is removed",
+            Data = id
+        });
     }
 }
