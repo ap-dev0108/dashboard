8aa35632dea76e58791725ab9eb411668cb5302a
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-05 10:14:03 +0545

    chore(migrations): Pushing initial migrations for DB and table creations

---
+++ server/Infrastructure/Migrations/20260805035642_InitialDBPush.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDBPush : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Media",
+                columns: table => new
+                {
+                    MediaID = table.Column<Guid>(type: "uuid", nullable: false),
+                    MediaTitle = table.Column<string>(type: "text", nullable: false),
+                    MediaDescription = table.Column<string>(type: "text", nullable: false),
+                    ImageURL = table.Column<string>(type: "text", nullable: false),
+                    Ratings = table.Column<int>(type: "integer", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Media", x => x.MediaID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Media");
+        }
+    }
+}
