73173baf15352216e23112ff866f8c884929ac3d
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-05 19:40:02 +0545

    chore(infrastructure): Pushing pending default folders

--- server/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ server/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f964354888db30df81ab781844ca7692da45a9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88c495b43fae938c7c4fd2ffaad1a39bd3de4f85")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
