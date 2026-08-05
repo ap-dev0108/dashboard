e993aa46307038a81b575ebdba33616740f1c9ae
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-05 15:02:20 +0545

    chore(migrations): Pushing migrations for finance creation

---
+++ server/Infrastructure/Migrations/20260805071528_FinanceTableCreation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinanceTableCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Finances",
+                columns: table => new
+                {
+                    FinanceID = table.Column<Guid>(type: "uuid", nullable: false),
+                    FinanceNotes = table.Column<string>(type: "text", nullable: false),
+                    Amount = table.Column<double>(type: "double precision", nullable: false),
+                    Total = table.Column<double>(type: "double precision", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    financeType = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Finances", x => x.FinanceID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Finances");
+        }
+    }
+}
