b382db839d1121f2c394d49f70b236da3ecfde1e
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-05 19:10:04 +0545

    refactor(dto): Updating FinanceDTO according to the model

--- server/Application/DTO/FinanceDTO/FinanceDTO.cs
+++ server/Application/DTO/FinanceDTO/FinanceDTO.cs
@@ -6,10 +6,7 @@ public class FinanceDTO
 {
     public Guid FinanceID { get; set; } = Guid.NewGuid();
     public string FinanceNotes { get; set; } = string.Empty;
-    public double Amount { get; set; }
-    public double Total { get; set; }
+    public decimal Amount { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public FinanceType financeType { get; set; }
-    public IncomeType IncomeType { get; set; }
-    public ExpenseType ExpenseType { get; set; }
 }
