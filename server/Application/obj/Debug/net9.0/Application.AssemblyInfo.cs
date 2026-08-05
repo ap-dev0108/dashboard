947eb0f23654b1a390a5c94b5b84934a1625a04b
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-05 19:46:46 +0545

    chore(application): Pushing pending default folders

--- server/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ server/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f964354888db30df81ab781844ca7692da45a9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54086403d7abda6cefa9dce5adfef534cc85198e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
