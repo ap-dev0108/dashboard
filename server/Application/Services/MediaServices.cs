dce16678436e90a6d0222a6be20ca10b559e319f
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-05 10:11:20 +0545

    chore(services): Implementing logics for CRUD on Media Services

--- server/Application/Services/MediaServices.cs
+++ server/Application/Services/MediaServices.cs
@@ -3,20 +3,18 @@ namespace server.Application;
 public class MediaService
 {
     private readonly IMediaRepo _media;
+    private readonly IDataRepo _data;
 
-    public MediaService(IMediaRepo media)
+    public MediaService(IMediaRepo media, IDataRepo data)
     {
         _media = media;
+        _data = data;
     }
 
     public async Task<List<MediaDTO>> GetMediaDTOsAsync()
     {
-        var allmediaList = await _media.GetAllMediasAsync();
-
-        if (allmediaList.Count <= 0)
-        {
-            throw new KeyNotFoundException("Media list is 0");
-        }
+        var allmediaList = await _media.GetAllMediasAsync() ??
+            throw new KeyNotFoundException("Media not found");
 
         return allmediaList.Select(s => new MediaDTO
         {
@@ -28,5 +26,48 @@ public class MediaService
             Status = s.Status,
             Type = s.Type
         }).ToList();
+    }
+
+    public async Task AddMedia(AddMediaDTO mediaDto)
+    {
+        var newMedia = new Domain.Media
+        {
+            MediaTitle = mediaDto.MediaTitle,
+            MediaDescription = mediaDto.MediaDescription,
+            ImageURL = mediaDto.ImageURL,
+            Ratings = mediaDto.Ratings,
+            Type = mediaDto.Type,
+            Status = mediaDto.Status
+        };
+
+        await _media.AddMedia(newMedia);
+        await _data.SaveChangesAsync();
+    }
+
+    public async Task<MediaDTO> GetMediaById(Guid id)
+    {
+        var mediaExists = await _media.GetMediaById(id) ??
+            throw new KeyNotFoundException("Media with this ID cannot be found");
+
+        var media = new MediaDTO
+        {
+            MediaID = mediaExists.MediaID,
+            MediaTitle = mediaExists.MediaTitle,
+            MediaDescription = mediaExists.MediaDescription,
+            ImageURL = mediaExists.ImageURL,
+            Type = mediaExists.Type,
+            Status = mediaExists.Status
+        };
+
+        return media;
+    }
+
+    public async Task RemoveMedia(Guid mediaID)
+    {
+        var mediaToRemove = await _media.GetMediaById(mediaID) ??
+            throw new KeyNotFoundException("Media with this ID cannot be foudn");
+
+        await _media.RemoveMedia(mediaToRemove);
+        await _data.SaveChangesAsync();
     }
 }
