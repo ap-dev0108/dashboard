70c089de3f56a4ccd41de019a522301a5f4a7a1d
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-05 12:03:36 +0545

    chore(migrations): Pushing new migrations for updated datatypes on Media entity and creation for projects table

---
+++ server/Infrastructure/Migrations/20260805061722_CreateProjectsTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateProjectsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Ratings",
+                table: "Media",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ProjectsID = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProjectTitle = table.Column<string>(type: "text", nullable: false),
+                    LiveURL = table.Column<string>(type: "text", nullable: false),
+                    GithubURL = table.Column<string>(type: "text", nullable: false),
+                    projectType = table.Column<int>(type: "integer", nullable: false),
+                    ImageURL = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ProjectsID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Ratings",
+                table: "Media",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+    }
+}
