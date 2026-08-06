d934b1bc06768deb5f668f4d9c7672ccbd5641a9
Aryan <ap01n08@gmail.com>
2026-08-06 20:19:08 +0545

    fix(api): Fixing api url

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://localhost:5088/api",
+    baseURL: "http://localhost:5088/api",
     headers: {
         "Content-Type": "application/json",
     },
