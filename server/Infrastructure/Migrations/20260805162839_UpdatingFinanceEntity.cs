e3e3db0d0d6df0530b255b3dbfe2e41d83d4357c
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-08-06 10:03:40 +0545

    chore(migrations): Pushing migrations to create finances table

---
+++ server/Infrastructure/Migrations/20260805162839_UpdatingFinanceEntity.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatingFinanceEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "Finances");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Finances",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Amount",
+                table: "Finances",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Total",
+                table: "Finances",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
