00be46d589edf57bb3222ea660f15e4772a26f20
Aryan <ap01n08@gmail.com>
2026-08-07 11:24:12 +0545

    api: Injecting all the provided apis

--- client/src/api/project.ts
+++ client/src/api/project.ts
@@ -6,3 +6,28 @@ export const getProjects = async () : Promise<Response<ProjectTypes[]>> => {
     const response = await axios.get("/Project/allProjects");
     return response.data;
 }
+
+export const getProjectsById = async (projectID: string) : Promise<Response<ProjectTypes[]>> => {
+    const response = await axios.get(`/Project/projectId/${projectID}`);
+    return response.data;
+}
+
+export const getProjectByType = async (projectType: string) : Promise<Response<ProjectTypes[]>> => {
+    const response = await axios.get(`/Project/type/${projectType}`);
+    return response.data;
+}
+
+export const addProjects = async (project: ProjectTypes) : Promise<Response<ProjectTypes[]>> => {
+    const response = await axios.post("/Project/add", project);
+    return response.data;
+}
+
+export const removeProjects = async (projectID: string) : Promise<Response<ProjectTypes[]>> => {
+    const response = await axios.delete(`/Project/remove/${projectID}`);
+    return response.data;
+}
+
+export const editProjects = async (project: ProjectTypes) : Promise<Response<ProjectTypes[]>> => {
+    const response = await axios.put("/Project/editProjects", project);
+    return response.data;
+}
