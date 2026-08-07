524950ee5fb5af4f1221290fc42a8ab3de1afcb2
Aryan <ap01n08@gmail.com>
2026-08-07 11:27:41 +0545

    types: replicating project model from backend to frontend

--- client/src/types/ProjectTypes.ts
+++ client/src/types/ProjectTypes.ts
@@ -1,14 +1,14 @@
-export interface ProjectType {
-    ProjectTitle: string,
-    LiveURL: string,
-    GithubURL: string,
-    ImageURL: string,
-    Type: ProjectTypeEnum
-}
-
 export enum ProjectTypeEnum {
     WebApp = "Web App",
     MobileApp = "Mobile App",
     WixTemplates = "Wix Templates",
     Management = "Management",
+}
+
+export interface ProjectTypes {
+    ProjectTitle: string,
+    LiveURL: string,
+    GithubURL: string,
+    ImageURL: string,
+    Type: ProjectTypeEnum
 }
