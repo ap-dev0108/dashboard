76502e5e8b8db95724a51222d2308e603cbfd34d
Aryan <ap01n08@gmail.com>
2026-08-07 10:09:29 +0545

    routes: Adding routes to media

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { RootLayout } from "../layout/RootLayout";
 import AllFinances from "../pages/finance";
 import { createBrowserRouter } from "react-router-dom";
+import Medias from "../pages/media";
 
 export const AppRouter = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ export const AppRouter = createBrowserRouter([
         index: true,
         Component: AllFinances,
       },
+      {
+        path: "/media",
+        Component: Medias
+      }
     ],
   },
 ]);
