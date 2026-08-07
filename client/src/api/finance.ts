a483ce545c16ef3bcd11c2a8e64d9e8c71bfa57b
Aryan <ap01n08@gmail.com>
2026-08-07 10:10:10 +0545

    api: Injecting apis for finance CRUD

--- client/src/api/finance.ts
+++ client/src/api/finance.ts
@@ -1,9 +1,38 @@
 import type { Finance } from "../types/FinanceTypes";
+import type { Response } from "../types/response";
 import { api } from "./axios";
 
-export const getFinanceData = async () : Promise<Finance> => {
+export const getFinanceData = async () : Promise<Response<Finance[]>> => {
     const response = await api.get("/Finance/allFinances");
-    
-    console.log(response.data);
+    return response.data;
+}
+
+export const getFinanceDataById = async (financeID: string) : Promise<Response<Finance[]>> => {
+    const response = await api.get(`/Finance/financeId/${financeID}`);
+    return response.data;
+}
+
+export const getFinanceDataByType = async (financeType: string) : Promise<Response<Finance[]>> => {
+    const response = await api.get(`/Finance/financeType/${financeType}`)
+    return response.data;
+}
+
+export const addFinanceData = async (finance: Finance) : Promise<Response<Finance>> => {
+    const response = await api.post("/Finance/addFinances", finance);
+    return response.data;
+}
+
+export const removeFinanceData = async (financeID: string) : Promise<Response<Finance>> => {
+    const response = await api.delete(`/Finance/removeFinance/${financeID}`);
+    return response.data;
+}
+
+export const financialCalculation = async () : Promise<Response<Finance>> => {
+    const response = await api.get("/Finance/getFinanceData");
+    return response.data;
+}
+
+export const editFinanceData = async (finance: Finance) : Promise<Response<Finance>> => {
+    const response = await api.put("/Finance/editFinances", finance);
     return response.data;
 }
