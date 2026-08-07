0011a545c8bcf1b814e6da93e66d5b213db7767f
Aryan <ap01n08@gmail.com>
2026-08-07 10:15:22 +0545

    Injecting header in layout

--- client/src/layout/RootLayout.tsx
+++ client/src/layout/RootLayout.tsx
@@ -1,5 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "../components/Headers/Header";
 
 export const RootLayout = () => {
-  return <Outlet />;
+  return (
+    <div className="">
+      <Header />
+      <Outlet />
+    </div>
+  );
 };
