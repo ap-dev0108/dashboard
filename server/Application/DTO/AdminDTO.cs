b37354b9fc3614878f3ad0f1c70507e1878c86e9
Aryan <ap01n08@gmail.com>
2026-08-10 22:07:19 +0545

    chore(dto): creating dtos for admin

---
+++ server/Application/DTO/AdminDTO.cs
@@ -0,0 +1,7 @@
+namespace server.Application;
+
+public class AdminSettings
+{
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+}
