a723c8c0b656e0ae86d423d004b663cb065c3833
Aryan <ap01n08@gmail.com>
2026-08-12 09:12:56 +0545

    chore(dto): Creating token response

---
+++ server/Application/DTO/TokenDTO.cs
@@ -0,0 +1,10 @@
+namespace server.Application;
+
+public class TokenResponse
+{
+    public string AccessToken { get; set; } = string.Empty;
+
+    public string RefreshToken { get; set; } = string.Empty;
+
+    public DateTime AccessTokenExpiresAt { get; set; }
+}
