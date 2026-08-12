415232e34a8acc628821def279e412e20ef55b98
Aryan <ap01n08@gmail.com>
2026-08-12 09:11:59 +0545

    chore(dto): Creating dtos for login and user profile

--- server/Application/DTO/AdminDTO.cs
+++ server/Application/DTO/AdminDTO.cs
@@ -2,6 +2,17 @@ namespace server.Application;
 
 public class AdminSettings
 {
-    public string Email { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
+
+public class LoginDTO
+{
+    public string Username {get; set;} = string.Empty;
+    public string Password {get; set;} = string.Empty;
+}
+
+public class UserProfileDTO
+{
+    public string Username { get; set; } = string.Empty;
 }
