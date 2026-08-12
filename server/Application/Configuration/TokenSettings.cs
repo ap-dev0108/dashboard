e77366fd60312f0cf3319849cacc8fe656fde036
Aryan <ap01n08@gmail.com>
2026-08-12 09:10:40 +0545

    chore(token): Creating token settings

---
+++ server/Application/Configuration/TokenSettings.cs
@@ -0,0 +1,14 @@
+namespace server.Application.Configuration;
+
+public class TokenSettings
+{
+    public string Key { get; set; } = string.Empty;
+
+    public string Issuer { get; set; } = string.Empty;
+
+    public string Audience { get; set; } = string.Empty;
+
+    public int AccessTokenLifetimeMinutes { get; set; }
+
+    public int RefreshTokenLifetimeDays { get; set; }
+}
